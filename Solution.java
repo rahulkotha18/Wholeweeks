35fd701ebe0201bc74ea342e39d3bc8ec861d7ee
rahul kotha <41829950+rahulkotha18@users.noreply.github.com>
2019-06-27 11:03:33 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -59,18 +59,18 @@ class Solution
     public int getMonth(String month)
     {
         HashMap<String,Integer> hm=new HashMap<>();
-        hm.put("Jan",1);
-        hm.put("Feb",2);
-        hm.put("Mar",3);
-        hm.put("Apr",4);
+        hm.put("January",1);
+        hm.put("February",2);
+        hm.put("March",3);
+        hm.put("April",4);
         hm.put("May",5);
-        hm.put("Jun",6);
-        hm.put("Jul",7);
-        hm.put("Aug",8);
-        hm.put("Sep",9);
-        hm.put("Oct",10);
-        hm.put("Nov",11);
-        hm.put("Dec",12);
+        hm.put("June",6);
+        hm.put("July",7);
+        hm.put("August",8);
+        hm.put("September",9);
+        hm.put("October",10);
+        hm.put("November",11);
+        hm.put("December",12);
         return (int)hm.get(month);
     }
 
@@ -96,7 +96,7 @@ class Solution
     public static void main(String...args)
     {
         try{
-        System.out.println("Enter year, start_month, end_month, first_day\n (give months in short forms)\n eg: 2019 Mar Jul Wednesday");
+        System.out.println("Enter year, start_month, end_month, first_day\n (give months in short forms)\n eg: 2019 March July Wednesday");
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String line[]=br.readLine().split(" ");
        // Scanner sc=new Scanner(System.in);
